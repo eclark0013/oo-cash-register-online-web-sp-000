129aa4921c6b7ac7581619edcb0e09a1f74130fe
Eric Clark <eclark0013@gmail.com>
2019-06-26 13:23:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,24 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total, :discount
 
-  def initialize
+  def initialize(discount=nil)
     @total=0
+    @discount=discount
   end
+
+  def add_item
+  end
+
+  def apply_discount
+  end
+
+  def items
+    
+  end
+
+  def void_last_transaction
+  end
+    
 
 
 end
